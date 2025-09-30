f025e1524539c71e63085776fb54369a03e5dfb2
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-30 11:44:03 +0000

    Release 1.8.12

--- src/flagright/core/client_wrapper.py
+++ src/flagright/core/client_wrapper.py
@@ -22,10 +22,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "flagright/1.8.11",
+            "User-Agent": "flagright/1.8.12",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "flagright",
-            "X-Fern-SDK-Version": "1.8.11",
+            "X-Fern-SDK-Version": "1.8.12",
             **(self.get_custom_headers() or {}),
         }
         headers["x-api-key"] = self.api_key
